d3c4539123414744395c4e34593a97c485d9785a
NgugiMuthoni <ngugimuthoni43@gmail.com>
2020-02-28 12:18:06 +0300

    added bootbox for alert and prompt popups

--- js/main.js
+++ js/main.js
@@ -18,22 +18,46 @@ function getDrink() {
     var selectedToppings = document.getElementById("drinks").value;
     return parseInt(selectedToppings);
 }
-function getTotalAmount() {
-    var totalAmount = (getSize() + getCrust() + getToppings() + getDrink()) * getQuantity();
-    alert("You have Ordered" + " " + getQuantity("")  +  " pizza."  +  ""  +  " The Total Amount is kshs "  +  (totalAmount)  +  ""  +  " Thank you for your order welcome again.");
-    return totalAmount;
-}
+
 
 function output(){
-    if(getTotalAmount()>0){
-    prompt("enter your location")
-    alert("You delivery fee is 100/=")
-    alert("You order will be delivered shortly")
-    alert("Continue shoppping with us each and every time .")
-    }else{
-        alert("Hope to see you again")
+    let ourAmount = getTotalAmount()
+    console.log(ourAmount);
+    if(ourAmount){
+
+        var promptOptions = {
+            title: "Enter your name",
+            message: "You order will be delivered shortly! You delivery fee will be 100/=. ",
+            buttons: {
+              confirm: {
+                label: "Ok"
+              }
+            },
+            callback: function(result) {                
+                if (result === null) {                                             
+                  console.log("Prompt dismissed");                              
+                } else {
+                  console.log("Hi "+result);                          
+                }
+              }
+        };
+        bootbox.prompt(promptOptions)
+        
+    } else {
+        bootbox.alert("Hope to see you again")
     }
     
+}
+
+
+function getTotalAmount() {
+    let totalAmount = (getSize() + getCrust() + getToppings() + getDrink()) * getQuantity();
+    if(totalAmount){
+        bootbox.alert("You have Ordered" + " " + getQuantity("")  +  " pizza."  +  ""  +  " The Total Amount is kshs "  +  (totalAmount)  +  ""  +  " Thank you for your order welcome again.");
+    } else {
+        return false
+    }
+    return totalAmount;
 }
 
 function button(){
@@ -41,4 +65,15 @@ function button(){
     document.getElementById('data').innerHTML= document.getElementById('output')
     
 }
+
+$(document).on("click", ".show-alert", function(e) {
+    bootbox.prompt("Hello world!", function() {
+        // console.log("Alert Callback");
+    });
+    bootbox.alert("Hello world!", function() {
+        // console.log("Alert Callback");
+    });
+    e.stopPropagation(); 
+            return false;
+});
 
